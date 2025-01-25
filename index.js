df836f14dff64b999002b4233e766bcc9304bffd
AlenaDemon <drmkpr@mail.ru>
2025-01-25 11:47:01 +0400

    add index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Hello, Hexlet!");
-import _ from "lodash";
-console.log(_.last(['one', 'two']));
+import lastIndex from './src/index.js'
+const last = lastIndex(["one", "two"])
+console.log(last)
